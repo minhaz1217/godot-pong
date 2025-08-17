e8e808f1b1846085844b97257b0ae4dce3a97ec2
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-08-18 02:54:16 +0600

    scoring and resetting working

--- game.gd
+++ game.gd
@@ -2,15 +2,23 @@ extends Node2D
 
 @onready var player_1: Sprite2D = $Player1
 @onready var player_2: Sprite2D = $Player2
+@onready var score: Label = $CanvasLayer/score
+@onready var ball: CharacterBody2D = $ball
 
-
+var player1_score: int = 0
+var player2_score: int = 0
+var ball_starting_position
 func _enter_tree() -> void:
 	DebugOverlay.setup_debugger($DebugOverlay)
-
-func _ready() -> void:
-	pass
 	
 
+func _ready() -> void:
+	ball_starting_position = ball.position
+	ball.velocity.y = randi_range(50, 100)
+	
+func reset_game():
+	ball.position = ball_starting_position
+	
 func _physics_process(delta: float) -> void:
 	pass
 
@@ -19,7 +27,15 @@ func _process(delta: float) -> void:
 
 
 func _on_ball_touched_wall(wall_name: Variant) -> void:
-	if(wall_name == "left"):
+	if (wall_name == "left"):
+		player2_score += 1
 		print("LEFT")
-	elif(wall_name == "right"):
+	elif (wall_name == "right"):
+		player1_score += 1
 		print("RIGHT")
+	update_score()
+	reset_game()
+	
+
+func update_score() -> void:
+	score.text = str(player1_score) + " : " + str(player2_score)
