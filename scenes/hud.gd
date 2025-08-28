cd2811dcedd15d2c98cca3e9355d01593850a55e
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-08-29 03:20:41 +0600

    added start game button signal

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,1 +1,11 @@
 extends CanvasLayer
+@export var hide_after_game_starts: Array[Node]
+
+func start_game():
+	for element in hide_after_game_starts:
+		element.visible = false
+
+
+func _on_button_pressed() -> void:
+	start_game()
+	GlobalScript.game_state.emit(GlobalScript.GameState.START)
