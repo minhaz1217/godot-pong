b0fd0f6c714e529f6569b7a53a241c4c41038628
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-09-24 04:46:41 +0600

    added sound to count down timer

--- scenes/count_down.gd
+++ scenes/count_down.gd
@@ -6,6 +6,8 @@ signal timer_finished
 
 @onready var timer: Timer = $Timer
 @onready var label: Label = $Label
+@onready var count_down_sound: AudioStreamPlayer2D = $CountDownSound
+@onready var count_down_end: AudioStreamPlayer2D = $CountDownEnd
 
 var current_time = 0
 
@@ -13,13 +15,15 @@ func _ready() -> void:
 	current_time = time
 	timer.start(1)
 	label.set_text(str(current_time))
+	count_down_sound.play()
 	
 func _on_timer_timeout() -> void:
 	current_time -= 1
 	label.set_text(str(current_time))
 	if(current_time > 0):
-		print(current_time)
+		count_down_sound.play()
 		timer.start(1)
 	else:
+		count_down_end.play()
+		count_down_end.connect("finished", queue_free)
 		timer_finished.emit()
-		queue_free()
